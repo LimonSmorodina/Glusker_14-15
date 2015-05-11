79ef0dc9707be949963bbde5aa18861a8e0e066a
Andrey Nepsha <putridbeard@gmail.com>
2015-05-11 12:05:46 +0000

    17 practice added

---
+++ practice_17/y.tab.h
@@ -0,0 +1,92 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    WHILE = 258,
+    BOOL_VAL = 259,
+    _BEGIN = 260,
+    _END = 261,
+    VARIBLE = 262,
+    INT_CONST = 263,
+    INT_OPERATION = 264,
+    ASSIGNMENT = 265,
+    LOG_OPERATION = 266,
+    SEMICOLON = 267,
+    S_SCOPE = 268,
+    E_SCOPE = 269,
+    NOT = 270,
+    DO = 271
+  };
+#endif
+/* Tokens.  */
+#define WHILE 258
+#define BOOL_VAL 259
+#define _BEGIN 260
+#define _END 261
+#define VARIBLE 262
+#define INT_CONST 263
+#define INT_OPERATION 264
+#define ASSIGNMENT 265
+#define LOG_OPERATION 266
+#define SEMICOLON 267
+#define S_SCOPE 268
+#define E_SCOPE 269
+#define NOT 270
+#define DO 271
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
